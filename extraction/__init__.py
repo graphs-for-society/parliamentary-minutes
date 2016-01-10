e43fcf68c026f3e83ee1da7a25c5ce292691dffe
Osman Baskaya <thyme.exe@gmail.com>
2016-01-10 13:49:54 +0200

    structure changes

--- extraction/__init__.py
+++ extraction/__init__.py
@@ -3,10 +3,12 @@ import sys
 import re
 import ujson
 import codecs
+from collections import defaultdict as dd
 
 TURKISH_ALPHABET_UPPER = "[A-ZÇĞİÖÜŞ]"
 TURKISH_ALPHABET = "[a-zçğıöüş]"
 
+NODES = dd(set)
 
 name_surname = r"(((%s+ ){2,})\(\w+\))" % TURKISH_ALPHABET_UPPER
 
@@ -16,6 +18,30 @@ reaction_regex = ur"\(((.*) sıralarından (\w+))\)"
 # regex = re.compile("BURCU KÖKSAL \([^\)]*\) ")
 # print '\n'.join(element[0] for element in re.findall(name_surname, text))
 #regex = re.compile("(BURCU KÖKSAL \(\w+\).*(?=({})))".format(name_surname))
+
+
+def create_node_types():
+    outputs = []
+
+    # Create Milletvekili nodetype
+    d = dict()
+    d["action"] = "nodetype_create"
+    d["name"] = "Milletvekili"
+    d["image_as_icon"] = False
+    outputs.append(d)
+
+    # Create Parti nodetype
+    d = dict()
+    d["action"] = "nodetype_create"
+    d["name"] = "Parti"
+    # d["image_as_icon"] = False
+    outputs.append(d)
+
+    # Create Konuşma nodetype
+    d = dict()
+    d["action"] = "nodetype_create"
+    d["name"] = "Konuşma"
+    outputs.append(d)
 
 
 def get_party_names(party_text):
@@ -57,15 +83,76 @@ def create_reactions_data(input_file):
     records = read_scrape_data(input_file)
     outputs = []
     for record in records:
-        text = record["talk_script"]
+        # Read representative properties
         representative = record["rep_name"]
         representative_party = record["representative_party"]
+        if representative_party == "AK Parti":
+            representative_party = "AK PARTİ"
+        rep_profile_url = record.get("rep_profile_url", "")
+        rep_profile_image_url = record.get("rep_profile_image_url", "")
+
+        # Read talk properties
+        text = record["talk_script"]
         talk_id = record["talk_id"]
-        date = record["session_date"]
+        talk_subject = record["talk_subject"]
+        talk_name = "%s (ID: %s)" % (talk_subject, talk_id)
+        talk_date = record["session_date"]
+        talk_url = record.get("talk_url", "")
         term_id = record['term_id']
         matches = re.findall(reaction_regex, text, flags=re.U)
-        reactions = []
+
         for match in matches:
+            # Create party node if it doesn't exist
+            if representative_party not in NODES['Parti']:
+                NODES['Parti'].add(representative_party)
+                d = dict()
+                d["action"] = "node_create"
+                d["name"] = representative_party
+                d["type"] = "Parti"
+                # TODO: Party Image Link
+                outputs.append(d)
+
+            # Create representative node if it doesn't exist
+            if representative not in NODES['Milletvekili']:
+                NODES['Milletvekili'].add(representative)
+                d = dict()
+                d["action"] = "node_create"
+                d["name"] = representative
+                d["type"] = "Milletvekili"
+                d["reference"] = rep_profile_url
+                d["image"] = rep_profile_image_url
+                d['properties'] = {"is member of": representative_party}
+                outputs.append(d)
+
+                # Party to representative edge (is member of)
+                # d = dict()
+                # d["action"] = "edge_create"
+                # d["from_name"] = representative_party
+                # d["from_type"] = "Parti"
+                # d["name"] = "ÜYESİ"
+                # d["to_name"] = representative
+                # d["to_type"] = "Milletvekili"
+                # outputs.append(d)
+
+            # Create talk node
+            d = dict()
+            d["action"] = "node_create"
+            d["name"] = talk_name
+            d["type"] = "Konuşma"
+            d["properties"] = {"talk_date": talk_date, "talk_summary": "%s ..." % text[:100], "term_id": term_id}
+            d["reference"] = talk_url
+            outputs.append(d)
+
+            # Create an edge from representative to the talk
+            d = dict()
+            d["action"] = "edge_create"
+            d["from_name"] = representative
+            d["from_type"] = "Milletvekili"
+            d["name"] = "KONUŞTU"
+            d["to_name"] = talk_name
+            d["to_type"] = "Konuşma"
+            outputs.append(d)
+
             _, party_text, reaction = match
             parties = get_party_names(party_text)
             for party in parties:
@@ -74,32 +161,12 @@ def create_reactions_data(input_file):
                 d["from_name"] = party
                 d["from_type"] = "Parti"
                 d["name"] = reaction.upper()
-                d["to_name"] = talk_id
+                d["to_name"] = talk_name
                 d["to_type"] = "Konuşma"
-                d['properties'] = {"speech_date": date, "speech_url": "www.buralar_yesillenecek.com",
-                                   "term": term_id}
-                reactions.append(d)
+                #d['properties'] = {"speech_date": date, "speech_url": "www.buralar_yesillenecek.com",
+                #                  "term": term_id}
+                outputs.append(d)
 
-        if len(reactions) > 0:
-            d = dict()
-            d["action"] = "edge_create"
-            d["from_name"] = representative
-            d["from_type"] = "Milletvekili"
-            d["name"] = "KONUŞTU"
-            d["to_name"] = talk_id
-            d["to_type"] = "Konuşma"
-            reactions.append(d)
-
-            d = dict()
-            d["action"] = "edge_create"
-            d["from_name"] = representative_party.upper()
-            d["from_type"] = "Parti"
-            d["name"] = "ÜYESİ"
-            d["to_name"] = representative
-            d["to_type"] = "Milletvekili"
-            reactions.append(d)
-        if len(reactions) != 0:
-            outputs.append(reactions)
     create_output("extraction_output.json", outputs)
 
 
