f3b3db9f39caaee2db543f1148f0d8c297e778b4
onur gungor <onurgu@gmail.com>
2016-01-14 20:05:40 +0200

    a new function for exploring paths is added

--- graph/__init__.py
+++ graph/__init__.py
@@ -8,18 +8,48 @@ config = ConfigParser.ConfigParser()
 config.read("config.ini")
 
 api_key = config.get("api", "api_key")
-
-graph_id = "801746fa-e5bf-4c0b-9b8a-7d300f63cb88"
+graph_id = config.get("graph", "graph_id")
 
 from graphcommons import GraphCommons
-graphcommons = GraphCommons(api_key)
+api = GraphCommons(api_key)
 
 from graphcommons import Signal
+
+def get_nodes(graph, node_type):
+    return filter(lambda node: node.type == node_type, graph.nodes)
+
+def create_nodetype_dicts(nodes):
+    names = [party.name for party in nodes]
+    node_dict = dict(zip(names, nodes))
+    return node_dict
+
+def get_paths(rep_name, party):
+    graph = api.graphs(graph_id)
+    rep_nodes, parties = map(lambda node_type: get_nodes(graph, node_type), ["Milletvekili", "Parti"])
+    reps_dict, parties_dict = map(create_nodetype_dicts, [rep_nodes, parties])
+    party = unicode(party)
+    rep_name = unicode(rep_name)
+    party_id = parties_dict[party].id
+    rep_id = reps_dict[rep_name].id
+    d = {"from": rep_id, "to": party_id, "limit": 30}
+    response = api.get_path(graph_id, d)
+    response_nodes = response['nodes']
+    to_return = []
+    for r in response['paths']:
+        d = {}
+        # speecher = r['nodes'][0]
+        speech_id = r['nodes'][1]
+        speech = response_nodes[speech_id]
+        d['reference'] = speech['reference']
+        d['path_string'] = r['path_string']
+        to_return.append(d)
+
+    return to_return
 
 def upsert_edges(signals, chunk_size=100):
     for i in range(0, len(signals)/chunk_size + 1):
         #print signals[i*chunk_size:(i+1)*chunk_size]
-        graphcommons.update_graph(
+        api.update_graph(
             id=graph_id,
             signals=signals[i*chunk_size:(i+1)*chunk_size]
         )
@@ -40,9 +70,6 @@ def upsert_edges(signals, chunk_size=100):
 }
 '''
 
-def get_paths(a, b, c):
-    return [1, 2, 3]
-
 
 def create_signals(input_filename):
     result = []
@@ -58,7 +85,7 @@ def create_signals(input_filename):
 
 def create_new_graph(graph_name, subtitle, description):
 
-    graph = graphcommons.new_graph(
+    graph = api.new_graph(
         name=graph_name,
         subtitle=subtitle,
         description=subtitle,
@@ -75,7 +102,7 @@ if __name__ == "__main__":
     parser.add_argument("--graph_id", nargs="?")
     args = parser.parse_args()
 
-    print graphcommons.status()
+    print api.status()
 
     if args.input_filename:
         signals = create_signals(args.input_filename)
